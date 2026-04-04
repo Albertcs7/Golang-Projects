3d9e18ac70e44c6783b3451bf8b8a300ff7942d3
Albertcs7 <albertacs2004@gmail.com>
2026-04-04 16:29:41 +0530

    contact_manager finished

--- contact_manager/main.go
+++ contact_manager/main.go
@@ -5,44 +5,61 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-func addcontact(map1 map[string]int) {
+func addcontact(map1 map[string]int, reader *bufio.Reader) {
 	var key, val string
-	fmt.Println("ENter key and value:")
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter Name:")
 	key, _ = reader.ReadString('\n')
+	key = strings.TrimSpace(key)
+
+	fmt.Println("Enter Phone Number:")
 	val, _ = reader.ReadString('\n')
-	value, _ := strconv.Atoi(val)
+	val = strings.TrimSpace(val)
+
+	value, err := strconv.Atoi(val)
+	if err != nil {
+		fmt.Println("Invalid number!")
+		return
+	}
+
 	map1[key] = value
+	fmt.Println("Contact added successfully!")
 }
 
 func viewcontact(map1 map[string]int) {
 	fmt.Println("Contact:")
 	for index, value := range map1 {
-		fmt.Println(index, ".", value)
+		fmt.Println(index, "->", value)
 	}
 }
 
-func delcontact(map1 map[string]int) {
-
+func delcontact(map1 map[string]int, reader *bufio.Reader) {
+	fmt.Println("Enter name to delete:")
+	del, _ := reader.ReadString('\n')
+	del = strings.TrimSpace(del)
+	delete(map1, del)
+	fmt.Println(del, "deleted")
 }
 
 func main() {
+	map1 := make(map[string]int)
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		var choice int
+
 		fmt.Println("\n1.Add contact\n2.View contact\n3.Delete contact\n4.Exit")
 		fmt.Print("Enter a choice: ")
-		fmt.Scan(&choice)
-		map1 := make(map[string]int)
-
+		inp, _ := reader.ReadString('\n')
+		inp = strings.TrimSpace(inp)
+		choice, _ := strconv.Atoi(inp)
 		switch choice {
 		case 1:
-			addcontact(map1)
+			addcontact(map1, reader)
 		case 2:
 			viewcontact(map1)
 		case 3:
-			delcontact(map1)
+			delcontact(map1, reader)
 		case 4:
 			os.Exit(0)
 		default:
